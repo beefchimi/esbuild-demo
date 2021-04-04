4a7a883f7b8a57ed697d87d7740d85091b671b72
beefchimi <curtdulmage@gmail.com>
2021-04-03 21:34:29 -0400

    progress

---
+++ other.jsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+
+let Goodbye = () => <h2>Go home you are drunk.</h2>;
+console.log(Goodbye);
--- scripts/build.js
+++ scripts/build.js
@@ -3,10 +3,22 @@ const envProduction = process.env.NODE_ENV === "production";
 require("esbuild")
   .build({
     entryPoints: ["app.jsx"],
+    format: "esm",
     bundle: true,
     minify: envProduction,
     sourcemap: !envProduction,
     target: ["chrome58", "firefox57", "safari11", "edge16"],
-    outfile: "dist/app.js",
+    outdir: "dist",
   })
   .catch(() => process.exit(1));
+
+/*
+require("fs").writeFileSync("in.ts", "let x: number = 1");
+
+require("esbuild").buildSync({
+  entryPoints: ["in.ts"],
+  outfile: "out.js",
+});
+
+require("fs").readFileSync("out.js", "utf8");
+*/
